ea1377df5d3b6f844e1ba75e78d2943ba4522ea0
likitha77-epuri <likithaeepuri77@gmail.com>
2026-03-14 22:16:15 +0530

    color picker

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello JavaScript");
+let colorContainer = document.getElementById("colorContainer");
+let selected = document.getElementById("selected");
+
+function button1() {
+    colorContainer.style.backgroundColor = "#e0e0e0";
+    selected.textContent = "#e0e0e0";
+}
+
+function button2() {
+    colorContainer.style.backgroundColor = "#6fcf97";
+    selected.textContent = "#6fcf97";
+}
+
+function button3() {
+    colorContainer.style.backgroundColor = "#56ccf2";
+    selected.textContent = "#56ccf2";
+}
+
+function button4() {
+    colorContainer.style.backgroundColor = "#bb6bd9";
+    selected.textContent = "#bb6bd9";
+}
