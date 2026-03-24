01c46e85a94e8b507a679ecc3b1916ebdd2232a7
likitha77-epuri <likithaeepuri77@gmail.com>
2026-03-24 21:31:42 +0530

    object as value

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ let person = {
   firstName: "Rahul",
   lastName: "Attuluri",
   age: 28,
-  habits: ["Playing Chess", "Singing"],
+  habits: ["Playing Chess", "Singing", "Dancing"],
+  car: {
+    name: "Audi",
+    model: "A6",
+    color: "White",
+  },
 };
 
-console.log(person.habits);  // ["Playing Chess", "Singing"]
+console.log(person.car.name);  // Audi
 
-console.log(person.habits[0]);  // Playing Chess
-
-console.log(person["habits"][1]);  // Singing
+console.log(person.car["model"]);  // A6
