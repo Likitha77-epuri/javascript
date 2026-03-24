423289b3b9d1112cf1c24d8084ca24b32780e3eb
likitha77-epuri <likithaeepuri77@gmail.com>
2026-03-24 21:30:50 +0530

    array as value

--- script.js
+++ script.js
@@ -1,46 +1,12 @@
-let todoItemsContainer = document.getElementById("todoItemsContainer");
-let todoList = [
-  {
-    text: "Learn HTML"
-  },
-  {
-    text: "Learn CSS"
-  },
-  {
-    text: "Learn JavaScript"
-  }
-];
+let person = {
+  firstName: "Rahul",
+  lastName: "Attuluri",
+  age: 28,
+  habits: ["Playing Chess", "Singing"],
+};
 
-function createAndAppendTodo(todo) {
-  let todoElement = document.createElement("li");
-  todoElement.classList.add("todo-item-container", "d-flex", "flex-row");
-  todoItemsContainer.appendChild(todoElement);
+console.log(person.habits);  // ["Playing Chess", "Singing"]
 
-  let inputElement = document.createElement("input");
-  inputElement.type = "checkbox";
-  inputElement.id = "checkboxInput";
-  inputElement.classList.add("checkbox-input");
-  todoElement.appendChild(inputElement);
+console.log(person.habits[0]);  // Playing Chess
 
-  let labelContainer = document.createElement("div");
-  labelContainer.classList.add("label-container", "d-flex", "flex-row");
-  todoElement.appendChild(labelContainer);
-
-  let labelElement = document.createElement("label");
-  labelElement.setAttribute("for", "checkboxInput");
-  labelElement.classList.add("checkbox-label");
-  labelElement.textContent = todo.text;
-  labelContainer.appendChild(labelElement);
-
-  let deleteIconContainer = document.createElement("div");
-  deleteIconContainer.classList.add("delete-icon-container");
-  labelContainer.appendChild(deleteIconContainer);
-
-  let deleteIcon = document.createElement("i");
-  deleteIcon.classList.add("far", "fa-trash-alt", "delete-icon");
-  deleteIconContainer.appendChild(deleteIcon);
-}
-
-for (let todo of todoList) {
-  createAndAppendTodo(todo);
-}
+console.log(person["habits"][1]);  // Singing
