653e78724b37194f641ef7a07235cd49aff6f092
likitha77-epuri <likithaeepuri77@gmail.com>
2026-03-25 22:21:03 +0530

    random color generator

--- script.js
+++ script.js
@@ -1,31 +1,12 @@
-let firstNumberElement=document.getElementById("firstNumber");
-let secondNumberElement=document.getElementById("secondNumber");
-let userInputElement=document.getElementById("userInput");
-let gameResultElement=document.getElementById("gameResult");
-let successMessage="Congratulations! You got it right.";
-let tryAgainMessage="Please Try Again!";
+let bgColorsArray = ["#e75d7c","#b16cef","#53cca4","#efc84d","#628ef0", "#184b73","#883e7f","#ed048b"];
+let bgContainerElement=document.getElementById("bgContainer");
+let buttonElement=document.getElementById("button");
 
-function restartGame(){
-    let firstRandomNumber=Math.random()*100;
-    let secondRandomNumber=Math.random()*100;
-
-    firstNumberElement.textContent=Math.ceil(firstRandomNumber);
-    secondNumberElement.textContent=Math.ceil(secondRandomNumber);
-
-    gameResultElement.textContent="";
-    userInputElement.value="";
-}
-restartGame();
-function checkResult(){
-    let firstRandomInteger=parseInt(firstNumberElement.textContent);
-    let secondRandomInteger=parseInt(secondNumberElement.textContent);
-    let userEnteredSum=parseInt(userInputElement.value);
-    let sumOfRandomNumbers=firstRandomInteger+secondRandomInteger;
-    if(userEnteredSum===sumOfRandomNumbers){
-        gameResultElement.textContent=successMessage;
-        gameResultElement.style.backgroundColor="#028a0f";
-    }else{
-        gameResultElement.textContent=tryAgainMessage;
-        gameResultElement.style.backgroundColor="#1e217c";
-    }
+bgContainerElement.style.backgroundColor=bgColorsArray[0];
+function clickMe(){
+    let numberOfBgColors=bgColorsArray.length;
+    let randomBgColorIndex=Math.ceil(Math.random()*numberOfBgColors);
+    let randomBgColor=bgColorsArray[randomBgColorIndex];
+    bgContainerElement.style.backgroundColor=randomBgColor;
+    
 }
