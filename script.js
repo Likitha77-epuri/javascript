06335deb9d4aae41dd4b0e5302c20396ba253638
likitha77-epuri <likithaeepuri77@gmail.com>
2026-03-25 21:57:15 +0530

    addition game

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-let myArray = [1, 2, 3, 4];
+let firstNumberElement=document.getElementById("firstNumber");
+let secondNumberElement=document.getElementById("secondNumber");
+let userInputElement=document.getElementById("userInput");
+let gameResultElement=document.getElementById("gameResult");
+let successMessage="Congratulations! You got it right.";
+let tryAgainMessage="Please Try Again!";
 
-for (let eachItem of myArray) {
-   console.log(eachItem);
+function restartGame(){
+    let firstRandomNumber=Math.random()*100;
+    let secondRandomNumber=Math.random()*100;
+
+    firstNumberElement.textContent=Math.ceil(firstRandomNumber);
+    secondNumberElement.textContent=Math.ceil(secondRandomNumber);
+
+    gameResultElement.textContent="";
+    userInputElement.value="";
+}
+restartGame();
+function checkResult(){
+    let firstRandomInteger=parseInt(firstNumberElement.textContent);
+    let secondRandomInteger=parseInt(secondNumberElement.textContent);
+    let userEnteredSum=parseInt(userInputElement.value);
+    let sumOfRandomNumbers=firstRandomInteger+secondRandomInteger;
+    if(userEnteredSum===sumOfRandomNumbers){
+        gameResultElement.textContent=successMessage;
+        gameResultElement.style.backgroundColor="#028a0f";
+    }else{
+        gameResultElement.textContent=tryAgainMessage;
+        gameResultElement.style.backgroundColor="#1e217c";
+    }
 }
