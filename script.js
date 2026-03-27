8a9cee8accbc99d645366000ed602e51bde31a18
likitha77-epuri <likithaeepuri77@gmail.com>
2026-03-27 22:10:49 +0530

    access values of array

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-/*
- * Write your code here and log the output.
- */
-let array=["Orange",25,100,true,33.58]
-console.log(array)
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  // index
+  let index = parseInt(readLine());
+  // myArray
+  let myArray = [25, false, "green", "apple"];
+
+  /*
+   * Write your code here and log the output.
+   */
+   console.log(myArray[index])
+       
+   
+}
