0f1c7608b417c28fe6b06279ce6c56d048ac5ebb
likitha77-epuri <likithaeepuri77@gmail.com>
2026-03-29 20:23:42 +0530

    call function

--- script.js
+++ script.js
@@ -1,37 +1,10 @@
-"use strict";
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", (inputStdin) => {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", (_) => {
-  inputString = inputString
-    .trim()
-    .split("\n")
-    .map((str) => str.trim());
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
+// function greet
+function greet() {
+  return "Hello! Have a nice day";
 }
 
-function main() {
-  let input = readLine().replace(/'/g, '"');
-  // myArray
-  let myArray = JSON.parse(input);
-
-  /*
-   * Write your code here and log the output.
-   */
-   console.log(myArray[myArray.length-1])
-   myArray.pop();
-   console.log(myArray);
-}
+/*
+ * Write your code here and log the output.
+ */
+greet()
+console.log(greet())
