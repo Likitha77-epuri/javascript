1f4659526f1421c39d73b3628b671f3479842996
likitha77-epuri <likithaeepuri77@gmail.com>
2026-04-03 23:32:35 +0530

    recipe page

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-let profileDetails = {
-    imgSrc: "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/user-profile-img.png",
-    name: "RAHUL ATTULURI",
-    age: 25
+let recipeObj = {
+    title: "Tomato Pasta",
+    imgSrc: "https://d2clawv67efefq.cloudfront.net/ccbp-dynamic-webapps/recipe-img.png",
+    ingredients: ["Pasta", "Oil", "Onions", "Salt", "Tomato Pasta Sauce", "Cheese"]
 };
-let profileContainerElement = document.getElementById("profileContainer");
-profileContainerElement.classList.add("text-center", "d-flex", "flex-column", "justify-content-center");
-let imageContainerElement = document.getElementById("imgContainer");
-let imageElement = document.createElement("img");
-imageElement.setAttribute("src", profileDetails.imgSrc);
-imageElement.classList.add("profile-img");
-imageContainerElement.appendChild(imageElement);
+let ingredientList = recipeObj.ingredients;
 
-let nameElement = document.createElement("h1");
-nameElement.classList.add("profile-name");
-nameElement.textContent = profileDetails.name;
-profileContainerElement.appendChild(nameElement);
+let recipeTitleElement = document.getElementById("recipeTitle");
+let imgContainerElement = document.getElementById("imgContainer");
+let imgElement = document.getElementById("img");
+let ingredientListContainerElement = document.getElementById("ingredientListContainer");
 
-let ageElement = document.createElement("p");
-ageElement.classList.add("age");
-ageElement.textContent = "Age: " + profileDetails.age;
-profileContainerElement.appendChild(ageElement);
+recipeTitleElement.textContent = recipeObj.title;
+imgElement.setAttribute("src", recipeObj.imgSrc);
+imgElement.classList.add("w-180");
+imgContainerElement.appendChild(imgElement);
+
+for (let ingredient of ingredientList) {
+    let ingredientElement = document.createElement("li");
+    ingredientElement.textContent = ingredient;
+    ingredientElement.classList.add("ingredient");
+    ingredientListContainerElement.appendChild(ingredientElement);
+}
