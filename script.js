c9ac0237b90dbbcc2d7260f3fcb776ee2047fc00
likitha77-epuri <likithaeepuri77@gmail.com>
2026-04-25 23:31:48 +0530

    POST request

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-let options = {
-  method: "GET"
+let data = {
+  name: "Rahul",
+  gender: "Male",
+  email: "rahul@gmail.com",
+  status: "Active"
 };
 
-fetch("https://gorest.co.in/public-api/users", options);
+let options = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: "Bearer ACCESS-TOKEN"
+  },
+  body: JSON.stringify(data)
+};
+
+fetch("https://gorest.co.in/public-api/users", options)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(jsonData) {
+    console.log(jsonData);
+  });
