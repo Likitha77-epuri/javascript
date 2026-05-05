5116fe6ea47468543b4fb744d1e47d82d48ebf81
likitha77-epuri <likithaeepuri77@gmail.com>
2026-05-05 22:09:20 +0530

    fact about number

--- script.js
+++ script.js
@@ -1,68 +1,37 @@
-let searchInputEl = document.getElementById("searchInput");
+let userInputEl = document.getElementById('userInput');
 let spinnerEl = document.getElementById("spinner");
-let resultCountriesEl = document.getElementById("resultCountries");
+let factEl = document.getElementById("fact");
 
-let searchInputVal = "";
-let countriesList = [];
+function getFactOfEneteredNumber(event) {
+    if (event.key === "Enter") {
+        let userInputVal = userInputEl.value;
 
-function createAndAppendCountry(country) {
-    let countryEl = document.createElement("div");
-    countryEl.classList.add("country-card", "col-11", "col-md-5", "mr-auto", "ml-auto", "d-flex", "flex-row");
-    resultCountriesEl.appendChild(countryEl);
-
-    let countryFlagEl = document.createElement("img");
-    countryFlagEl.src = country.flag;
-    countryFlagEl.classList.add("country-flag", "mt-auto", "mb-auto");
-    countryEl.appendChild(countryFlagEl);
-
-    let countryInfoEl = document.createElement("div");
-    countryInfoEl.classList.add("d-flex", "flex-column", "ml-4");
-    countryEl.appendChild(countryInfoEl);
-
-    let countryNameEl = document.createElement("p");
-    countryNameEl.textContent = country.name;
-    countryNameEl.classList.add("country-name");
-    countryInfoEl.appendChild(countryNameEl);
-
-    let countryPopulationEl = document.createElement("p");
-    countryPopulationEl.textContent = country.population;
-    countryPopulationEl.classList.add("country-population");
-    countryInfoEl.appendChild(countryPopulationEl);
-}
-
-function displaySearchResults() {
-    resultCountriesEl.textContent = "";
-    for (let country of countriesList) {
-
-        let countryName = country.name;
-
-        if (countryName.includes(searchInputVal)) {
-            createAndAppendCountry(country);
+        if (userInputVal === "") {
+            alert("Enter a Number");
+            return;
         }
+
+        let url = "https://apis.ccbp.in/numbers-fact?number=" + userInputVal;
+        let options = {
+            method: "GET"
+        };
+
+        spinnerEl.classList.remove("d-none");
+        factEl.classList.add("d-none");
+
+        fetch(url, options)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(jsonData) {
+                factEl.classList.remove("d-none");
+                spinnerEl.classList.add("d-none");
+
+                let {
+                    fact
+                } = jsonData;
+                factEl.textContent = fact;
+            });
     }
 }
-
-function getCountries() {
-    let url = "https://apis.ccbp.in/countries-data";
-    let options = {
-        method: "GET",
-    };
-
-    spinnerEl.classList.remove("d-none");
-    fetch(url, options)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(jsonData) {
-            spinnerEl.classList.add("d-none");
-            countriesList = jsonData;
-            displaySearchResults();
-        });
-}
-
-function onChangeSearchInput(event) {
-    searchInputVal = event.target.value;
-    displaySearchResults();
-}
-getCountries();
-searchInputEl.addEventListener("keyup", onChangeSearchInput);
+userInputEl.addEventListener("keyup", getFactOfEneteredNumber);
