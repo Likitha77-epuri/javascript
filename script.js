ba092a793120dfb235b5aa477fa3132a0926821e
likitha77-epuri <likithaeepuri77@gmail.com>
2026-05-11 19:22:10 +0530

    user

--- script.js
+++ script.js
@@ -1,61 +1,98 @@
-let consoleFormEl = document.getElementById("consoleForm");
-let requestUrlEl = document.getElementById("requestUrl");
-let responseStatusEl = document.getElementById("responseStatus");
-let requestUrlErrMsgEl = document.getElementById("requestUrlErrMsg");
-let requestMethodEl = document.getElementById("requestMethod");
-let requestBodyEl = document.getElementById("requestBody");
-let responseBodyEl = document.getElementById("responseBody");
+let addUserFormEl = document.getElementById("addUserForm");
+let nameEl = document.getElementById("name");
+let emailEl = document.getElementById("email");
+let nameErrMsgEl = document.getElementById("nameErrMsg");
+let emailErrMsgEl = document.getElementById("emailErrMsg");
+let statusEl = document.getElementById("status");
+let genderMaleEl = document.getElementById("genderMale");
+let genderFemaleEl = document.getElementById("genderFemale");
 
-function checkRequestUrl() {
-    if (requestUrlEl.value === "") {
-        requestUrlErrMsgEl.textContent = "Required*";
-        requestUrlErrMsgEl.classList.add("error-message");
-    } else {
-        requestUrlErrMsgEl.textContent = "";
-    }
-}
 let formData = {
-    requestUrl: "https://gorest.co.in/public-api/users",
-    requestMethod: "POST",
-    requestBody: ""
-}
-requestUrlEl.addEventListener("change", function(event) {
-    formData.requestUrl = event.target.value;
+    name: "",
+    email: "",
+    status: "Active",
+    gender: "male"
+};
+nameEl.addEventListener("change", function(event) {
+    if (event.target.value === "") {
+        nameErrMsgEl.textContent = "Required*";
+    } else {
+        nameErrMsgEl.textContent = "";
+    }
+    formData.name = event.target.value;
 });
-requestMethodEl.addEventListener("change", function(event) {
-    formData.requestMethod = event.target.value;
+emailEl.addEventListener("change", function(event) {
+    if (event.target.value === "") {
+        emailErrMsgEl.textContent = "Required*";
+    } else {
+        emailErrMsgEl.textContent = "";
+    }
+    formData.email = event.target.value;
 });
-requestBodyEl.addEventListener("change", function(event) {
-    formData.requestBody = event.target.value;
+statusEl.addEventListener("change", function(event) {
+    formData.status = event.target.value;
+});
+genderMaleEl.addEventListener("change", function(event) {
+    formData.gender = event.target.value;
+});
+genderFemaleEl.addEventListener("change", function(event) {
+    formData.gender = event.target.value;
 });
 
-function sendHTTPRequest() {
+function validateFormData(formData) {
     let {
-        requestUrl,
-        requestMethod,
-        requestBody
+        name,
+        email
     } = formData;
-    let url = requestUrl;
+    let isValid = true;
+    if (name === "") {
+        nameErrMsgEl.textContent = "Required*";
+        isValid = false;
+    } else {
+        nameErrMsgEl.textContent = "";
+    }
+    if (email === "") {
+        emailErrMsgEl.textContent = "Required*";
+        isValid = false;
+    } else {
+        emailErrMsgEl.textContent = "";
+    }
+    return isValid;
+}
+
+function submitFormData(formData) {
     let options = {
-        method: requestMethod,
+        method: "POST",
         headers: {
             "Content-Type": "application/json",
             Accept: "application/json",
+            Authorization: "Bearer 9f401acddfb1dce3"
         },
-        body: requestBody
+        body: JSON.stringify(formData)
     };
+    let url = "https://gorest.co.in/public-api/users";
     fetch(url, options)
         .then(function(response) {
             return response.json();
         })
         .then(function(jsonData) {
-            let requestStatus = jsonData.code;
-            responseStatusEl.value = requestStatus;
-            responseBodyEl.textContent = JSON.stringify(jsonData);
+            console.log(jsonData);
+            if (jsonData.code === 422) {
+                let errorMsg = jsonData.data[0].message;
+
+                if (errorMsg === "has already been taken") {
+                    emailErrMsgEl.textContent = "Email already exists";
+                }
+
+            } else if (jsonData.code === 201) {
+                alert("User created successfully");
+            }
         });
 }
-consoleFormEl.addEventListener("submit", function(event) {
+addUserFormEl.addEventListener("submit", function(event) {
     event.preventDefault();
-    checkRequestUrl();
-    sendHTTPRequest();
-})
+    let isValid = validateFormData(formData);
+    if (isValid) {
+        submitFormData(formData);
+    }
+});
