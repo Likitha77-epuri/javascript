17181ae7a7c7eee41a3a60d95bba3a5ab37eb3a1
likitha77-epuri <likithaeepuri77@gmail.com>
2026-05-12 22:05:02 +0530

    bookmark maker

--- script.js
+++ script.js
@@ -1,61 +1,64 @@
-let consoleFormEl = document.getElementById("consoleForm");
-let requestUrlEl = document.getElementById("requestUrl");
-let responseStatusEl = document.getElementById("responseStatus");
-let requestUrlErrMsgEl = document.getElementById("requestUrlErrMsg");
-let requestMethodEl = document.getElementById("requestMethod");
-let requestBodyEl = document.getElementById("requestBody");
-let responseBodyEl = document.getElementById("responseBody");
+// Example Valid URLs: https://learning.ccbp.in/, https://www.google.com/
+let bookmarkForm = document.getElementById("bookmarkForm");
+let siteNameInput = document.getElementById("siteNameInput");
+let siteUrlInput = document.getElementById("siteUrlInput");
+let siteNameErrMsg = document.getElementById("siteNameErrMsg");
+let submitBtn = document.getElementById("submitBtn");
+let bookmarksList = document.getElementById("bookmarksList");
+let siteUrlErrMsg = document.getElementById("siteUrlErrMsg");
 
-function checkRequestUrl() {
-    if (requestUrlEl.value === "") {
-        requestUrlErrMsgEl.textContent = "Required*";
-        requestUrlErrMsgEl.classList.add("error-message");
+function addBookMark() {
+    let siteName = siteNameInput.value;
+    let siteURL = siteUrlInput.value;
+    let listItem = document.createElement("li");
+    bookmarksList.appendChild(listItem);
+    let bookmarkName = document.createElement("p");
+    let bookmarkUrl = document.createElement("a");
+    bookmarkName.textContent = siteName;
+    bookmarkUrl.textContent = siteURL;
+    bookmarkUrl.href = siteURL;
+    bookmarkUrl.setAttribute("target", "_blank");
+    bookmarksList.classList.remove("d-none");
+    listItem.appendChild(bookmarkName);
+    listItem.appendChild(bookmarkUrl);
+}
+
+siteUrlInput.addEventListener("change", function(event) {
+    if (event.target.value === "") {
+        siteUrlErrMsg.textContent = "Required*";
     } else {
-        requestUrlErrMsgEl.textContent = "";
+        siteUrlErrMsg.textContent = "";
     }
-}
-let formData = {
-    requestUrl: "https://gorest.co.in/public-api/users",
-    requestMethod: "POST",
-    requestBody: ""
-}
-requestUrlEl.addEventListener("change", function(event) {
-    formData.requestUrl = event.target.value;
-});
-requestMethodEl.addEventListener("change", function(event) {
-    formData.requestMethod = event.target.value;
-});
-requestBodyEl.addEventListener("change", function(event) {
-    formData.requestBody = event.target.value;
 });
 
-function sendHTTPRequest() {
-    let {
-        requestUrl,
-        requestMethod,
-        requestBody
-    } = formData;
-    let url = requestUrl;
-    let options = {
-        method: requestMethod,
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-        },
-        body: requestBody
-    };
-    fetch(url, options)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(jsonData) {
-            let requestStatus = jsonData.code;
-            responseStatusEl.value = requestStatus;
-            responseBodyEl.textContent = JSON.stringify(jsonData);
-        });
-}
-consoleFormEl.addEventListener("submit", function(event) {
+siteNameInput.addEventListener("change", function(event) {
+    if (event.target.value === "") {
+        siteNameErrMsg.textContent = "Required*";
+    } else {
+        siteNameErrMsg.textContent = "";
+    }
+});
+
+bookmarkForm.addEventListener("submit", function(event) {
     event.preventDefault();
-    checkRequestUrl();
-    sendHTTPRequest();
-})
+
+    let siteName = siteNameInput.value.trim();
+    let siteURL = siteUrlInput.value.trim();
+    let isValid = true;
+    if (siteName === "") {
+        siteNameErrMsg.textContent = "Required*";
+        isValid = false;
+    }
+    if (siteURL === "") {
+        siteUrlErrMsg.textContent = "Required*";
+        isValid = false;
+    } else if (!(siteURL.startsWith("https://") || siteURL.startsWith("http://"))) {
+        siteUrlErrMsg.textContent = "Invalid URL";
+        isValid = false;
+    }
+    if (isValid) {
+        addBookMark();
+        siteNameInput.value = "";
+        siteUrlInput.value = "";
+    }
+});
